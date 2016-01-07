7f1456f0b572b34c6f55fa57fe3809aaef90b2d3
atranarta <radyuk.tatyana@gmail.com>
2016-01-07 19:57:56 +0200

    object, methods

--- GoIT/javascript/home/oleg_zmijuk/Lesson3-4/task1.js
+++ GoIT/javascript/home/oleg_zmijuk/Lesson3-4/task1.js
@@ -1,32 +1,3 @@
-//var body = document.querySelector('body');
-//
-//var newOl = document.createElement('ol');
-//body.appendChild(newOl);
-//
-//for (var j = 1; j <= 3; j++) {
-//    var li = document.createElement('li');
-//    li.innerHTML = "Вопрос №" + j;
-//
-//    for (var i = 1; i <= 3; i++) {
-//        var label = document.createElement('label');
-//        li.appendChild(label);
-//        var checkBox = document.createElement('input');
-//        checkBox.type = 'checkbox';
-//        var labelSpan = document.createElement('span');
-//        label.appendChild(checkBox);
-//        label.appendChild(labelSpan);
-//        labelSpan.innerHTML = "Вариант ответа №" + i;
-//    };
-//    
-//    newOl.appendChild(li);
-//
-//};
-//
-//var button = document.createElement('button');
-//button.innerHTML = "Проверить мои результаты";
-//body.appendChild(button);
-
-
 var app = {
     createElement: function(params) {
         var element = document.createElement(params.tagName);
@@ -35,8 +6,13 @@ var app = {
             element.setAttribute('type', params.inputType)
         } 
     
-        element.className = params.className;
-        element.innerHTML = params.content;
+        if (params.className) {
+            element.className = params.className;
+        }
+        
+        if (params.content) {
+            element.innerHTML = params.content;
+        }
     
         if (params.parentElement) {
             params.parentElement.appendChild(element);
@@ -47,28 +23,22 @@ var app = {
     
     generateQuestions: function(questionsAmount, answerAmount) {
         for (var i = 1; i <= questionsAmount; i++) {
-            this.createElement({
-                tagName: 'h2',
+            var li = this.createElement({
+                tagName: 'li',
                 content: 'Вопрос №' + i,
-                parentElement: body
+                parentElement: ol
             })
         
             for (var j = 1; j <= answerAmount; j++) {
                 var label = this.createElement({
                     tagName: 'label',
-                    content: 'Вариант ответа №' + j,
-                    parentElement: body
-                })
-                this.createElement({
-                    tagName: 'input',
-                    inputType: 'checkbox',
-                    parentElement: label
+                    content: '<input type="checkbox" /> Вариант ответа №' + j,
+                    parentElement: li
                 })
             }
         }
     }
 };
-
 
 var body = document.querySelector('body');
 
@@ -79,6 +49,16 @@ app.createElement({
     parentElement: body
 });
 
+var form = app.createElement({
+    tagName: 'form',
+    parentElement: body
+});
+
+var ol = app.createElement({
+    tagName: 'ol',
+    parentElement: form
+});
+
 app.generateQuestions(3, 3);      
 
 app.createElement({
@@ -86,7 +66,8 @@ app.createElement({
     inputType: 'submit',
     className: 'check-results',
     content: 'Проверить мои результаты',
-    parentElement: body
+    parentElement: form
 });
+
 
 
