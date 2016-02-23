e66c2c460d8dd1f7172c0a1a4226dabc9c989aba
atranarta <radyuk.tatyana@gmail.com>
2016-02-23 21:16:10 +0100

    Lesson 15-16

--- GoIT/javascript/home/oleg_zmijuk/Lesson 15-16/script/script.js
+++ GoIT/javascript/home/oleg_zmijuk/Lesson 15-16/script/script.js
@@ -12,9 +12,7 @@ jQuery(document).ready(function () {
                 jQuery.each(data.results,
                     function (i, val) {
                         var li = document.createElement("li");
-
                         li.innerHTML = '<a href="' + val.url + '" title="' + val.url + '" target="_blank">' + val.title + "</a> - " + val.content;
-
                         ul.appendChild(li);
                     }
                 );
@@ -29,3 +27,58 @@ jQuery(document).ready(function () {
 function GoogleCallback(func, data) {
     window[func](data);
 }
+
+
+
+
+
+var Human = {
+    name: 'Valeriy',
+    age: 28,
+    sex: 'male',
+    height: 178,
+    weight: 73
+};
+
+var Worker = {
+    job: 'Flixbus',
+    salary: 75000,
+    toWork: function () {
+        alert('Give me my computer! I need some work!');
+    }
+};
+
+var Student = {
+    university: 'LP',
+    payment: 500,
+    toWatch: function () {
+        alert('I don`t want to study!');
+    }
+};
+
+Worker.__proto__ = Human;
+Student.__proto__ = Human;
+
+var Tanya = Object.create(Worker, {
+    name: {
+        value: 'Tanya'
+    },
+    salary: {
+        value: 30000
+    },
+    age: {
+        value: 26
+    },
+});
+
+var Katya = Object.create(Student);
+Katya.name = 'Katya';
+Katya.age = 18;
+Katya.weight = 58;
+
+console.log('Human ', Human);
+console.log('Worker ', Worker);
+console.log('Student ', Student);
+
+console.log(Tanya.name + ' is ' + Tanya.age + ' years old and she wants to work in ' + Tanya.job + ' and receive ' + Tanya.salary + ' €/month');
+console.log(Katya.name + ' is a student. She is ' + Katya.age + ' and she studies at the ' + Katya.university + '. She receives ' + Katya.payment + ' UAH/month. ' + Katya.name + ' is poor but happy.');
